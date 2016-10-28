d398983c2ae4d4f7bb0a9ed9c69e88b3ffb63353
Cesar <cesardespinoza@unitec.edu>
2016-10-28 17:31:07 -0600

    s

--- Laboratorio3.cpp
+++ Laboratorio3.cpp
@@ -6,8 +6,8 @@ int*** crearCalendario();
 int*** crearMatriz(int);
 
 int main() {
-  int menu=0;
-  while (menu!=4) {
+  int menu=2;
+  do {
     cout<<"     Laboratorio 3\n"<<"1)Ejercicio1\n2)Ejercicio2\n3)Ejercicio3\n4)Salir"<<endl;
     int menu;
     cin>>menu;
@@ -18,16 +18,13 @@ int main() {
         dias_semana[i]=i;
       }
       int* semana = new int[7];
-
       for (int i = 0; i < 100; i++) {
         for (int j = 0; j < 12; j++) {
           if (i % 4 == 0 && j==2) {
             for (int k = 0; k <= 29; k++) {
               for (int z = 0; z < 7; z++) {
                 int temp;
-
                 semana[z]=k;
-
               }
               cout<<k<<" ";
             }
@@ -36,37 +33,42 @@ int main() {
         }
         cout<<endl<<i<<" ";
       }
-
     }
     if(menu==2){
       int grado_poli;
       cout<<"Ingrese el grado mas alto del polinomio: ";
-
       cin>>grado_poli;
-      int polinomio[grado_poli];
-      int division_sintetica[grado_poli-1];
+      int polinomio[3][grado_poli];
       int binomio;
-      for (int i = grado_poli; i >= 0; i--) {
-        cout<<"Ingrese el polinomio x^"<<i<<": ";
-        cin>>polinomio[i];
+      int cont = grado_poli;
+      for (int i = 0; i <= grado_poli; i++) {
+        cout<<"Ingrese el polinomio x^"<<cont<<": ";
+        cin>>polinomio[0][i];
         cout<<endl;
+        cont --;
       }
       cout<<"Ingrese a: ";
       cin>>binomio;
       cout<<endl;
-      division_sintetica[0] = polinomio[0];
-      for (int i = 0; i < grado_poli; i++) {
-        division_sintetica[i] = ((division_sintetica[i])*binomio)+polinomio[i];
-        cout<<division_sintetica[i]<<endl;
+      for (int i = 0; i <= 3; i++) {
+        for (int j = 0; j <= grado_poli; j++) {
+          if (i==0) {
+            cout<<polinomio[i][j]<<" ";
+          }
+          polinomio[i][j]=(polinomio[i][j]*binomio)+ (polinomio[i][j]);
+          if(i==1){
+            cout<<polinomio[i][j]<<" ";
+          }
+        }
+        cout<<endl;
       }
     }
-
     if(menu==3){
-      /*cout<<"Permutacion"<<endl;
+      cout<<"Permutacion"<<endl;
       cout<<"Ingrese un numero que no tenga mas de 4 cifras";
       char num;
       cin>> num;
-      for (int i = 0; i < 5; i++) {
+      /*for (int i = 0; i < 5; i++) {
         char temp = num[i];
         if((i+1)<5){
           num[i+1]= num[i];
@@ -77,7 +79,7 @@ int main() {
         cout<< num[i];
       }*/
     }
-  }
+  }while (menu!=4);
   return 0;
 }
 
