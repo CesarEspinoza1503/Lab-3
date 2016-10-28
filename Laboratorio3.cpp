2a27fb292bdb53af9f1c848dbf997d31ab775c0c
Cesar <cesardespinoza@unitec.edu>
2016-10-28 15:28:46 -0600

    copia

--- Laboratorio3.cpp
+++ Laboratorio3.cpp
@@ -5,49 +5,75 @@ using namespace std;
 int*** crearCalendario();
 int*** crearMatriz(int);
 
-int int main(int argc, char const *argv[]) {
+int main() {
   int menu=0;
   while (menu!=4) {
-    cout<<"     Laboratorio 3"<<"1)Ejercicio1\n2)Ejercicio2\n3)Ejercicio3\n4)Salir"<<endl;
+    cout<<"     Laboratorio 3\n"<<"1)Ejercicio1\n2)Ejercicio2\n3)Ejercicio3\n4)Salir"<<endl;
     int menu;
     cin>>menu;
     if(menu==1){
       cout<<"Domingos que cayeron primer dia del mes durante el siglo 20"<<endl;
+      int* dias_semana = new int[7];
+      for (int i = 1; i <= 7; i++) {
+        dias_semana[i]=i;
+      }
+      int* semana = new int[7];
+
+      for (int i = 0; i < 100; i++) {
+        for (int j = 0; j < 12; j++) {
+          if (i % 4 == 0 && j==2) {
+            for (int k = 0; k <= 29; k++) {
+              for (int z = 0; z < 7; z++) {
+                int temp;
+
+                semana[z]=k;
+
+              }
+              cout<<k<<" ";
+            }
+          }
+          cout<<endl<<j<<" ";
+        }
+        cout<<endl<<i<<" ";
+      }
 
     }
     if(menu==2){
+      int grado_poli;
+      cout<<"Ingrese el grado mas alto del polinomio: ";
+      int*** sintetica = crearMatriz(grado_poli);
 
+      cin>>grado_poli;
     }
     if(menu==3){
-
+      cout<<"Permutacion"<<endl;
+      cout<<"Ingrese un numero que no tenga mas de 4 cifras";
+      char num;
+      cin>> num;
+      for (int i = 0; i < 5; i++) {
+        char temp = num[i];
+        if((i+1)<5){
+          num[i+1]= num[i];
+        }
+        if((i+1)>5){
+          num[1]= num[i];
+        }
+        cout<< num[i];
+      }
     }
   }
   return 0;
 }
 
-int*** crearCalendario(){
-  int*** Calendario = new int [100][12][31];
-  for (int i = 0; i < 100; i++) {
-    for (int j = 0; j < 12; j++) {
-      for (int k = 0; k < 31; k++) {
-        if (j==4 || j==6 || j==9 || j==11) {
-          while (k<31) {
-            Calendario[i][j][k]=k;
-          }
-        }else{
-          Calendario[i][j][k]=k;
-        }
-        if (i % 4 ==0 && j==2) {
-          while (k<30) {
-            Calendario[i][j][k]=k;
-          }
-        }
-        if (j==2) {
-          while (k<29) {
-            Calendario[i][j][k]=k;
-          }
-        }
-      }
+int*** crearMatriz(int size){
+  int*** matriz = new int**[size];
+  for (int i = 0; i < size; i++) {
+    matriz[i]=new int*[3];
+  }
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < 3; j++) {
+      matriz[i][j]= new int[size];
     }
   }
+  return matriz;
 }
